7a9960188ad1b25447170781434254921318c364
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-01-10 22:15:12 +0530

    Revised about lists, tuple, set and dictonary and solved some practice questions.

---
+++ Python/Lecture 6/practice.py
@@ -0,0 +1,25 @@
+"""## List Programs"""
+
+# Create 2 lists from a given list where
+# 1st list will contain all the odd numbers from the original list and
+# the 2nd one will contain all the even numbers
+
+L = [1,2,3,4,5,6]
+
+# How to take list as input from user
+
+# Write a program to merge 2 list without using the + operator
+L1 = [1,2,3,4]
+L2 = [5,6,7,8]
+
+# Write a program to replace an item with a different item if found in the list
+L = [1,2,3,4,5,3]
+# replace 3 with 300
+
+# Write a program that can convert a 2D list to 1D list
+
+# Write a program to remove duplicate items from a list
+
+L = [1,2,1,2,3,4,5,3,4]
+
+# Write a program to check if a list is in ascending order or not
