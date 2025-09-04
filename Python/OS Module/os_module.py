51aff2cccaf43c1861bfdbdf9593550ccd12fa63
Shriram Vibhute <shriram.vibhute08@gmail.com>
2025-09-04 20:32:28 +0530

    refactor: clean up notebooks and add file renaming functionality
    - Remove empty code cell from Embedded Methods notebook
    - Reset execution count in Hybrid Naive Bayes notebook
    - Add file renaming utility in os_module.py to organize Miscellaneous directory

--- Python/OS Module/os_module.py
+++ Python/OS Module/os_module.py
@@ -30,3 +30,14 @@ print(os.getcwd())
 import os
 os.chdir('/Programming')
 print(os.getcwd())
+
+
+import os
+import regex as re
+
+if os.path.exists('9 - Miscellaneous'):
+    for i in os.listdir('9 - Miscellaneous'):
+        match = re.match(r'\d+\s-\s(.*)', i) 
+        if match: 
+            new_name = match.group(1)
+            os.rename('9 - Miscellaneous/' + i, '9 - Miscellaneous/' + new_name)
